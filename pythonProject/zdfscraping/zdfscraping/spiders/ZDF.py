e018639ffaa1f262b936ed3b00ba48a3808a3f8b
uw61ehod <changgeng.drewes@fau.de>
2024-05-18 21:43:01 +0200

    uploading something

--- pythonProject/zdfscraping/zdfscraping/spiders/ZDF.py
+++ pythonProject/zdfscraping/zdfscraping/spiders/ZDF.py
@@ -1,4 +1,27 @@
+from typing import Any
+
 import scrapy
 
+
 class ZDF(scrapy.Spider):
-    pass
+    name = "zdf_scrapy"
+    start_urls = ["https://www.zdf.de/nachrichten"]
+
+    def parse(self, response):
+        for link in [response.urljoin(sublink) for sublink in response.css("div.i13b5qgu a ::attr(href)").extract()]:
+            yield response.follow(link, callback=self.linkToNews)
+
+    def linkToNews(self, response):
+        for link in [response.urljoin(sublink) for sublink in response.css('a.f1mro3s7.sn98z8j._nl_::attr(href)').extract()]:
+            yield response.follow(link, callback=self.getNews)
+
+    def getNews(self, response):
+        yield {
+            "Link": response,
+            "News Category" : response.css('span.o1byiadb.t1ktg2ut ::text').extract(),
+            "publish_time": response.css('span.djkus7a.m211382 ::text').extract(),
+            "Title" : response.css('span.t1w1kov8.hhhtovw ::text').extract(),
+            "scribe": response.css('p.ikh9v7p.c1bdz7f4 ::text').extract(),
+            "Text": response.css('div.s1am5zo.f1uhhdhr ::text').extract()
+        }
+
